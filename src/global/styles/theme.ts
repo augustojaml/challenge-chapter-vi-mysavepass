bf3bd2992f518eb32da2882f4ebe0e96320ddf74
augustojaml <jamonteirolima@gmail.com>
2022-04-24 15:58:37 -0300

    challenge chapter vi

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,51 @@
+export const theme = {
+  dark: {
+    theme: 'dark',
+    colors: {
+      background: '#121214',
+      background_secondary: '#202024',
+      background_tertiary: '#8257e6',
+
+      background_item: '#353535',
+
+      warning: '#ffc107',
+      success: '#28a745',
+      danger: '#dc3545',
+
+      text_light: '#ffffff',
+      text_dark: '#b2b2b2',
+      text_gray_dark: '#595959',
+      text_gray: '#b2b2b2',
+    },
+    fonts: {
+      regular: 'Poppins_400Regular',
+      medium: 'Poppins_600SemiBold',
+      bold: 'Poppins_700Bold',
+    },
+  },
+
+  light: {
+    theme: 'light',
+    colors: {
+      background: '#e2e4e9',
+      background_secondary: '#1967fb',
+      background_tertiary: '#eeb111',
+
+      background_item: '#ffffff',
+
+      warning: '#ffc107',
+      success: '#28a745',
+      danger: '#dc3545',
+
+      text_light: '#ffffff',
+      text_dark: '#000000',
+      text_gray_dark: '#595959',
+      text_gray: '#b2b2b2',
+    },
+    fonts: {
+      regular: 'Montserrat_400Regular',
+      medium: 'Montserrat_600SemiBold',
+      bold: 'Montserrat_700Bold',
+    },
+  },
+};
